e48bb645013da3e28ae43831eaf8c75686a66320
Pablo Fontaine <pablofontaine97@gmail.com>
2022-10-23 23:18:05 -0300

    Correction models.py, migrate and makemigrations

--- first_mvt/dispenser/models.py
+++ first_mvt/dispenser/models.py
@@ -3,12 +3,12 @@ from django.db import models
 # Create your models here.
 
 class Dispenser(models.Model):
-    serial_number = models.CharField(max_lenght=10)
-    machine_type = models.CharField(max_lenght=10)
-    brand = models.CharField(max_lenght=10)
-    model = models.CharField(max_lenght=10)
-    color = models.CharField(max_lenght=10)
-    purchase = models.CharField(max_lenght=10)
+    serial_number = models.CharField(max_length=10)
+    machine_type = models.CharField(max_length=10)
+    brand = models.CharField(max_length=10)
+    model = models.CharField(max_length=10)
+    color = models.CharField(max_length=10)
+    purchase = models.CharField(max_length=10)
 
     def __str__(self, serial_number: str, machine_type: str, brand: str, model: str, color: str, purchase: str):
         return f'''
