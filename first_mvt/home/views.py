577384a325051a9ecb290b638a78e21d967886a5
Pablo Fontaine <pablofontaine97@gmail.com>
2022-10-30 17:50:42 -0300

    I added a form to search for family members in the home template. This form searches with three parameters (first name, last name and relationship) and returns results. If results exist, it returns objects that match the search parameters.

--- first_mvt/home/views.py
+++ first_mvt/home/views.py
@@ -1,4 +1,9 @@
+from django.db.models import Q
+from django.contrib import messages
 from django.shortcuts import render
+
+from family.models import Family_member
+
 
 # Create your views here.
 
@@ -7,4 +12,29 @@ def index(request):
         request=request,
         context={},
         template_name="home/index.html",
+    )
+
+def search(request):
+    search_param = request.GET['search_param']
+    context_dict = dict()
+
+    if search_param:
+        query = Q(name__contains=search_param)
+        print(query)
+        query.add(Q(relationship__contains=search_param), Q.OR)
+        print(query)
+        query.add(Q(last_name__contains=search_param), Q.OR)
+        print(query)
+        members = Family_member.objects.filter(query)
+        context_dict.update(
+            {
+                'members': members,
+                'search_param': search_param,
+            }
+        )
+
+    return render(
+        request=request,
+        context=context_dict,
+        template_name='home/index.html'
     )
