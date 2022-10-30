a1cf31329d929618f09f2d25520ad7be5c871508
Pablo Fontaine <pablofontaine97@gmail.com>
2022-10-30 18:09:26 -0300

    Add adventure and posession models

--- first_mvt/family/models.py
+++ first_mvt/family/models.py
@@ -1,4 +1,3 @@
-from unicodedata import name
 from django.db import models
 
 # Create your models here.
@@ -11,3 +10,18 @@ class Family_member(models.Model):
 
     def __str__(self):
         return f'Nombre: {self.name} | Apellido: {self.last_name} | Edad: {self.age} | Relación: {self.relationship}'
+
+class Adventure(models.Model):
+    place = models.CharField(max_length=40)
+    date = models.DateField()
+
+    def __str__(self) -> str:
+        return f'Fecha {self.date} | Lugar {self.place}'
+
+class Possession(models.Model):
+    item = models.CharField(max_length=40)
+    value = models.IntegerField()
+    owner = models.CharField(max_length=40)
+
+    def __str__(self) -> str:
+        return f'Bien {self.item} vale {self.value} y es de {self.owner}'
